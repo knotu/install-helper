68797d80fb4be099521a1381b63051c62d4d1f64
knotu <113001659+knotu@users.noreply.github.com>
2022-09-28 16:41:46 -0400

    Update arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -1,22 +1,6 @@
 #!/usr/bin/env bash
 
-pkgs=(xorg-server xorg-xdpyinfo xorg-xev xorg-xinit xorg-xinput xorg-xkill xorg-xrandr xorg-xsetroot xorg-xwayland
-
-linux-headers git ntfs-3g openssh refind
-
-jwm terminator pcmanfm lxappearance lxqt-notificationd lxrandr nitrogen dmenu redshift scrot xterm
-
-firefox opera opera-ffmpeg-codecs vivaldi vivaldi-ffmpeg-codecs
-
-moc mpv pcsx2 steam
-
-atool bash-completion btop ffmpeg ffmeg4.4 htop nano ncdu newsboat pulsemixer rsync weechat xbindkeys xcolor xdotool yt-dlp
-
-galculator gimp keepassxc meld scite veracrypt virtualbox virtualbox-guest-additions
-
-nvidia-dkms nvidia-settings
-
-noto-fonts noto-fonts-emoji)
+pkgs=(xorg-server xorg-xdpyinfo xorg-xev xorg-xinit xorg-xinput xorg-xkill xorg-xrandr xorg-xsetroot xorg-xwayland linux-headers git ntfs-3g openssh refind jwm terminator pcmanfm lxappearance lxqt-notificationd lxrandr nitrogen dmenu redshift scrot xterm firefox opera opera-ffmpeg-codecs vivaldi vivaldi-ffmpeg-codecs moc mpv pcsx2 steam atool bash-completion btop ffmpeg ffmeg4.4 htop nano ncdu newsboat pulsemixer rsync weechat xbindkeys xcolor xdotool yt-dlp galculator gimp keepassxc meld scite veracrypt virtualbox virtualbox-guest-additions nvidia-dkms nvidia-settings noto-fonts noto-fonts-emoji)
 
 pacman --noconfirm --needed -S  ${pkgs}
 #enable services
