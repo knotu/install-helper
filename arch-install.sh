6147829b516597b6519db91f3dd9cb17f586bd95
knotu <113001659+knotu@users.noreply.github.com>
2022-09-28 16:40:05 -0400

    Update arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
-pkgs=(xorg-server xorg-xdpyinfo xorg-xev xorg-xinit xorg-xinput xorg-xkill xorg-xrandr xorg-xsetroot xorg-xwayland)
+pkgs=(xorg-server xorg-xdpyinfo xorg-xev xorg-xinit xorg-xinput xorg-xkill xorg-xrandr xorg-xsetroot xorg-xwayland
 
-pkgs=(linux-headers git ntfs-3g openssh refind)
+linux-headers git ntfs-3g openssh refind
 
-pkgs=(jwm terminator pcmanfm lxappearance lxqt-notificationd lxrandr nitrogen dmenu redshift scrot xterm)
+jwm terminator pcmanfm lxappearance lxqt-notificationd lxrandr nitrogen dmenu redshift scrot xterm
 
-pkgs=(firefox opera opera-ffmpeg-codecs vivaldi vivaldi-ffmpeg-codecs)
+firefox opera opera-ffmpeg-codecs vivaldi vivaldi-ffmpeg-codecs
 
-pkgs=(moc mpv pcsx2 steam)
+moc mpv pcsx2 steam
 
-pkgs=(atool bash-completion btop ffmpeg ffmeg4.4 htop nano ncdu newsboat pulsemixer rsync weechat xbindkeys xcolor xdotool yt-dlp)
+atool bash-completion btop ffmpeg ffmeg4.4 htop nano ncdu newsboat pulsemixer rsync weechat xbindkeys xcolor xdotool yt-dlp
 
-pkgs=(galculator gimp keepassxc meld scite veracrypt virtualbox virtualbox-guest-additions)
+galculator gimp keepassxc meld scite veracrypt virtualbox virtualbox-guest-additions
 
-pkgs=(nvidia-dkms nvidia-settings)
+nvidia-dkms nvidia-settings
 
-pkgs=(noto-fonts noto-fonts-emoji)
+noto-fonts noto-fonts-emoji)
 
 pacman --noconfirm --needed -S  ${pkgs}
 #enable services
