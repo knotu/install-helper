badde6e3bcbb4b58d26089eeb4a8607fd6d6dff5
knotu <113001659+knotu@users.noreply.github.com>
2022-09-28 16:45:22 -0400

    Update arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -2,7 +2,7 @@
 
 pkgs=(xorg-server xorg-xdpyinfo xorg-xev xorg-xinit xorg-xinput xorg-xkill xorg-xrandr xorg-xsetroot xorg-xwayland linux-headers git ntfs-3g openssh refind jwm terminator pcmanfm lxappearance lxqt-notificationd lxrandr nitrogen dmenu redshift scrot xterm firefox opera opera-ffmpeg-codecs vivaldi vivaldi-ffmpeg-codecs moc mpv pcsx2 steam atool bash-completion btop ffmpeg ffmeg4.4 htop nano ncdu newsboat pulsemixer rsync weechat xbindkeys xcolor xdotool yt-dlp galculator gimp keepassxc meld scite veracrypt virtualbox virtualbox-guest-additions nvidia-dkms nvidia-settings noto-fonts noto-fonts-emoji)
 
-pacman --noconfirm --needed -S  $pkgs
+pacman --noconfirm -S  $pkgs
 #enable services
 sudo systemctl enable sshd
 sudo systemctl start sshd
