d0e7465f51586e8546f65d180629025ddbee1632
knotu <113001659+knotu@users.noreply.github.com>
2022-10-12 23:28:19 -0400

    added some echo commands

--- arch-install.sh
+++ arch-install.sh
@@ -1,20 +1,34 @@
 #!/usr/bin/env bash
 
-echo "terminator &" > ~/.xinitrc
-echo "exec jwm" > ~/.xinitrc
+
+{
+echo 'PATH=$PATH:$HOME/bin' 
+echo 
+echo 'shopt -s autocd' 
+echo "PROMPT_COMMAND='[[ ${__new_wd:=$PWD} != $PWD ]] && ls -CF;__new_wd=$PWD' # ls after cding" 
+echo 
+echo 'alias q='exit'' 
+}  >> ~/.bashrc
+
+echo "xterm &" >> ~/.xinitrc
+echo "exec jwm" >> ~/.xinitrc
+
+
+### Install ###
+
 
 # xorg
-pacman --needed -S xorg-server xorg-xdpyinfo xorg-xev xorg-xinit xorg-xinput xorg-xkill xorg-xrandr xorg-xsetroot xorg-xwayland xorg-xev
+pacman --needed -S xorg-server xorg-xdpyinfo xorg-xev xorg-xinit xorg-xinput xorg-xkill xorg-xrandr xorg-xsetroot xorg-xwayland
 # core
-pacman --needed -S linux-headers git ntfs-3g openssh refind nvidia-dkms nvidia-settings
+pacman --needed -S linux-headers git ntfs-3g openssh udisks2 refind nvidia-dkms nvidia-settings
 # desktop
-pacman --needed -S jwm terminator pcmanfm lxappearance lxqt-notificationd lxrandr nitrogen dmenu redshift scrot xterm 
+pacman --needed -S jwm pcmanfm dunst feh dmenu xterm 
 # browser
-pacman --needed -S firefox opera opera-ffmpeg-codecs vivaldi vivaldi-ffmpeg-codecs 
+pacman --needed -S firefox vivaldi vivaldi-ffmpeg-codecs 
 # entertainment
 pacman --needed -S moc mpv pcsx2 steam 
 # cli
-pacman --needed -S atool bash-completion btop ffmpeg ffmpeg4.4 htop imagemagick micro  ncdu newsboat pulsemixer rsync weechat xbindkeys xcolor xdotool yt-dlp 
+pacman --needed -S atool bash-completion btop ffmpeg ffmpeg4.4 imagemagick micro ncdu newsboat pulsemixer rsync weechat xbindkeys xcolor xdotool yt-dlp 
 # gui and other
 pacman --needed -S galculator gimp gparted keepassxc meld scite veracrypt virtualbox virtualbox-guest-utils noto-fonts noto-fonts-emoji ttf-liberation
 
@@ -23,6 +37,6 @@ systemctl enable --now sshd
 
 echo
 echo
-echo " After installing rEFInd and running refind-install add 'nvidia-drm.modeset=1 ibt=off' to kernel parameters in /boot/refind_linux.conf"
+echo "After installing rEFInd and running refind-install add 'nvidia-drm.modeset=1 ibt=off' to kernel parameters in /boot/refind_linux.conf"
 echo
 echo
