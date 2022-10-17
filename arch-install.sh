0792372fe8a222ef409508f851a27df9aa92363a
knotu <113001659+knotu@users.noreply.github.com>
2022-10-16 21:53:35 -0400

    changes to applications

--- arch-install.sh
+++ arch-install.sh
@@ -18,17 +18,17 @@ echo "exec jwm" >> ~/.xinitrc
 
 
 # xorg
-pacman --needed -S xorg-server xorg-xdpyinfo xorg-xev xorg-xinit xorg-xinput xorg-xkill xorg-xrandr xorg-xsetroot xorg-xwayland
+pacman --needed -S xorg-server xorg-xdpyinfo xorg-xev xorg-xinit xorg-xinput xorg-xkill xorg-xrandr xorg-xsetroot
 # core
 pacman --needed -S linux-headers git ntfs-3g openssh udisks2 refind nvidia-dkms nvidia-settings
 # desktop
-pacman --needed -S jwm pcmanfm dunst feh dmenu xterm 
+pacman --needed -S jwm pcmanfm dunst dmenu xterm xwallpaper  
 # browser
 pacman --needed -S firefox vivaldi vivaldi-ffmpeg-codecs 
 # entertainment
 pacman --needed -S moc mpv pcsx2 steam 
 # cli
-pacman --needed -S atool bash-completion btop ffmpeg ffmpeg4.4 imagemagick micro ncdu newsboat pulsemixer rsync weechat xbindkeys xcolor xdotool yt-dlp 
+pacman --needed -S atool bash-completion btop ffmpeg ffmpeg4.4 imagemagick micro ncdu newsboat pulsemixer rsync weechat xbindkeys xdotool yt-dlp 
 # gui and other
 pacman --needed -S galculator gimp gparted keepassxc meld scite veracrypt virtualbox virtualbox-guest-utils noto-fonts noto-fonts-emoji ttf-liberation
 
