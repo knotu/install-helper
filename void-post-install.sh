2929b882637db7dfcaf38e06b8d12ba98a615a38
knotu <113001659+knotu@users.noreply.github.com>
2023-03-20 12:01:16 -0400

    script rework

--- void-post-install.sh
+++ void-post-install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# update the system
+sudo xbps-install -Suy
+
 # add nonfree repo for things like steam
 sudo xbps-install -Sy void-repo-nonfree
 
@@ -7,5 +10,37 @@ sudo xbps-install -Sy void-repo-nonfree
 sudo xbps-install -Syv void-repo-multilib{,-nonfree}
 sudo xbps-install -Syv void-repo-multilib
 
-sudo xbps-install atool base-devel bash-completion bashmount btop dmenu dunst ffmpeg firefox gparted ImageMagick jwm keepassxc liberation-fonts-ttf meld micro moc mpv mtpaint ncdu newsboat noto-fonts-emoji noto-fonts-ttf pcmanfm pipewire pulsemixer rsync scite steam sxhkd weechat xdotool xorg-fonts xorg-minimal xrdb xterm xwallpaper yt-dlp
+# development
+sudo xbps-install base-devel libX11-devel libXft-devel libXinerama-devel
+
+# xorg-minimal and graphics
+sudo xbps-install xorg-minimal corectrl mesa-dri mesa-vaapi mesa-vdpau xf86-video-amdgpu vulkan-loader mesa-vulkan-radeon 
+
+# desktop and gui based 
+sudo xbps-install dmenu dunst firefox gparted jwm keepassxc meld moc mpv mtpaint pcmanfm scite steam synergy
+
+# audio and video
+sudo xbps-install elogind ffmpeg moc mpv pipewire pulsemixer 
+
+# cli based
+sudo xbps-install alacritty atool bash-completion bashmount btop ImageMagick micro ncdu newsboat rsync sxhkd weechat xdotool xrdb xterm stools xwallpaper yt-dlp
+
+# fonts
+sudo xbps-install liberation-fonts-ttf noto-fonts-emoji noto-fonts-ttf xorg-fonts
+
+# enable dbus
+sudo ln -s /etc/sv/dbus /var/service
+
+# enable elogind
+sudo ln -s /etc/sv/elogind /var/service
+
+## XBPS source packages (for building)
+# clone repo
+
+git clone --depth=1 https://github.com/void-linux/void-packages.git
+
+#cd void-packages
+#./xbps-src binary-bootstrap
+# xbps-src update-check
+# 
 
