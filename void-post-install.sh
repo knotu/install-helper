85dd4893f22bfc5ec2c0bf4a1a49574a25585698
knotu <113001659+knotu@users.noreply.github.com>
2023-03-20 03:04:04 -0400

    various changes

--- void-post-install.sh
+++ void-post-install.sh
@@ -1,39 +1,11 @@
 #!/usr/bin/env bash
 
 # add nonfree repo for things like steam
-xbps-install -Sy void-repo-nonfree
+sudo xbps-install -Sy void-repo-nonfree
 
 # add multilib for things like lib32 mesa
-xbps-install -Syv void-repo-multilib{,-nonfree}
-xbps-install -Syv void-repo-multilib
+sudo xbps-install -Syv void-repo-multilib{,-nonfree}
+sudo xbps-install -Syv void-repo-multilib
 
-sudo xbps-install xorg base-devel jwm specrtwm pcmanfm dunst dmenu xterm xwallpaper firefox moc mpv mtpaint atool bash-completion btop ffmpeg ImageMagick micro ncdu newsboat pulsemixer rsync weechat sxhkd xdotool yt-dlp gparted keepassxc meld scite steam liberation-fonts-ttf noto-fonts-ttf noto-fonts-emoji pipewire
-
-{
-echo 'PATH=$PATH:$HOME/bin' 
-echo 
-echo 'shopt -s autocd' 
-echo "PROMPT_COMMAND='[[ ${__new_wd:=$PWD} != $PWD ]] && ls -CF;__new_wd=$PWD' # ls after cding" 
-echo 
-echo 'alias q='exit'' 
-}  >> ~/.bashrc
-
-mkdir ~/.config/sxhkd
-
-{
-echo 'super + r'
-echo	 'dmenu_run'
-echo
-echo 'alt + t'
-echo	 'alacritty'
-echo
-echo 'super + period'
-		 'xdotool set_desktop --relative 1'
-echo 'super + comma'
-echo 	 'xdotool set_desktop --relative -1'
-
-} >> ~/.config/sxhkd/sxhkdrc
- 
-echo "xterm &" >> ~/.xinitrc
-echo "exec spectrwm" >> ~/.xinitrc
+sudo xbps-install atool base-devel bash-completion bashmount btop dmenu dunst ffmpeg firefox gparted ImageMagick jwm keepassxc liberation-fonts-ttf meld micro moc mpv mtpaint ncdu newsboat noto-fonts-emoji noto-fonts-ttf pcmanfm pipewire pulsemixer rsync scite steam sxhkd weechat xdotool xorg-fonts xorg-minimal xrdb xterm xwallpaper yt-dlp
 
