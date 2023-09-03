eb2b0008761d8d5837087236243b2835e8101542
Knotu <113001659+knotu@users.noreply.github.com>
2023-09-03 10:57:29 -0400

    various changes to void script

--- void-pkg-install.sh
+++ void-pkg-install.sh
@@ -10,42 +10,45 @@ set -e
 
 ## updating and setting up repos
 
-# add nonfree repo(steam)
-xbps-install -Sy void-repo-nonfree void-repo-multilib{,-nonfree}
-
 # update the system
 xbps-install -Suy
 xbps-install -u xbps
 
+# add nonfree repo(steam)
+xbps-install -Sy void-repo-nonfree void-repo-multilib{,-nonfree}
+
 # kernel
-xbps-install linux linux-headers
+xbps-install linux-mainline linux-mainline-headers
 
-# development
-xbps-install base-devel libX11-devel libXft-devel libXinerama-devel
+# devel
+xbps-install base-devel git libX11-devel libXcursor-devel libXft-devel libXinerama-devel libXrandr-devel libXt-devel libXtst-devel libevdev-devel libxkbcommon-devel meson ninja pango-devel pixman-devel pkg-config scdoc wayland-devel wlroots-devel xcb-util-devel xcb-util-keysyms-devel xcb-util-wm-devel zig
 
-# xorg related and X related
-xbps-install xorg-minimal xorg-fonts xev xdotool xkill xprop xrandr xrdb xset xsetroot xterm xtool xorg-fonts
+# xorg related
+xbps-install xorg-minimal xclip xcolor xdotool xev xeyes xhost xkill xinput xorg-fonts xprop xrandr xrdb xset xsetroot xterm
 
-# wayland related (river installed manually)
-xbps-install wayland-devel wayland-protocols libevdev-devel wlroots-devel scdoc pkg-config xorg-server-xwayland shfmt wev i3blocks i3status grim wlr-randr wf-recorder swaybg swayimg slurp foot bemenu wev wtype
+# wayland related 
+xbps-install bemenu kanshi wayland-protocols xorg-server-xwayland wev grim wlr-randr wlrctl wf-recorder sway swaybg swayimg slurp foot wev wtype
 
 # graphic and related
 xbps-install mesa-dri mesa-vaapi xf86-video-amdgpu vulkan-loader mesa-vulkan-radeon libdrm-32bit libglvnd-32bit mesa-dri-32bit mesa-vulkan-radeon-32bit vulkan-loader-32bit
 
 # desktop and gui based 
-xbps-install dmenu dunst firefox gparted jwm keepassxc meld moc mpv mtpaint pcmanfm scite steam synergy sxhkd
+xbps-install dmenu dunst feh firefox galculator gparted i3blocks i3status jwm keepassxc meld pcmanfm scite steam synergy sxhkd vivaldi
 
 # audio and video
-xbps-install ffmpeg moc mpv pipewire pulsemixer alsa-plugins-pulseaudio
+xbps-install alsa-plugins-pulseaudio ffmpeg moc mpv pipewire pulsemixer
 
 # cli based
-xbps-install alacritty atool bash-completion bashmount btop feh ImageMagick lm_sensors micro ncdu newsboat opendoas psmisc rsync shellcheck vivid xtools weechat yt-dlp
+xbps-install 7zip alacritty atool bash-completion bashmount btop curl ImageMagick lm_sensors micro ncdu newsboat opendoas psmisc rsync shellcheck shfmt udiskie vivid weechat wget yt-dlp zip
 
-# unsorted various
-xbps-install elogind libgcc-32bit libstdc++-32bit gtk+ imlib2 vpm vsv xmirror
+# void utils & wrappers
+xbps-install vpm vsv xmirror xtools 
 
 # fonts
 xbps-install noto-fonts-emoji noto-fonts-ttf 
+
+# needs sorted
+xbps-install elogind libgcc-32bit libstdc++-32bit gtk+ imlib2 refind qemu
 
 # enable dbus and elogind services
 ln -s /etc/sv/dbus /var/service
